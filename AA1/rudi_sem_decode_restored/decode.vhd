af36b8afd75c27f4ad7bbfda8b8c8d649605e293
hiago ritter santos <hiago.s2006@aluno.ifsc.edu.br>
2026-08-04 11:28:32 -0300

    Rudi - Caminho de Dados

--- AA1/rudi_sem_decode_restored/decode.vhd
+++ AA1/rudi_sem_decode_restored/decode.vhd
@@ -13,7 +13,30 @@ entity decode is
   );
 end entity;
 
+-- Implementacao do decode da estrutura 
 architecture struct of decode is
 begin
 
+	process(opcode)
+	begin
+		case opcode is
+			when "00" => 
+				sel <= 'X';
+				wr_acc <= '0';
+				op <= 'X';
+			when "01" => 
+				op <= 'X';
+				sel <= '0'; 
+				wr_acc <='1';
+			when "10" => 
+				sel <= '1'; 
+				wr_acc <= '1'; 
+				op <= '0';
+			when "11" => 
+				sel <= '1'; 
+				wr_acc <= '1'; 
+				op <= '1';
+		end case;
+	end process;
+		
 end architecture;
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# AOC
+# AOC - Arquitetura e organização de computadores
+
+Matéria ministrada pelo professor Roberto de Matos no semestre 2026.2 no IFSC SJ.
+
+Repositório para armazenar atividades e anotações das aulas de AOC.
+
+Sinta-se a vontade para utilizar dos materiais aqui presentes.
